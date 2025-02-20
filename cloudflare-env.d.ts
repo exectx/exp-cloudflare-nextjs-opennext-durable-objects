8535398cf3677eda7078e8e139f50180bbfd4203
exectx <81214874+exectx@users.noreply.github.com>
2025-02-19 21:58:33 -0500

    experiment durable objects + nextjs + static assets

--- cloudflare-env.d.ts
+++ cloudflare-env.d.ts
@@ -2,4 +2,5 @@
 // by running `wrangler types --env-interface CloudflareEnv cloudflare-env.d.ts`
 
 interface CloudflareEnv {
+  Counter: DurableObjectNamespace<import("./src/workers/do").Counter>;
 }
---
+++ src/workers/do-wrapper.js
@@ -0,0 +1,4 @@
+import nextapp from "./worker.js";
+import { Counter } from "../src/workers/do.js";
+export default nextapp;
+export { Counter };
